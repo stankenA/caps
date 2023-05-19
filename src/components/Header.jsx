7f2f5e2c28fdbb6f3034fbb3e9f2a5742ddbf99a
stanken_a <artyemst3@mail.ru>
2023-05-19 13:32:44 +0300

    feat: complete header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
+import { Link } from 'react-router-dom';
+
+import logo from '../images/caps-logo.svg';
 
 export default function Header() {
 
   const projectsArr = ['СЗППК СПБ/РЖД', 'DEL ARTE', 'SPARTA', 'MK STROY', 'UNIQCKATE'];
+
+  const menuList = [
+    {
+      title: 'Услуги',
+      link: '/'
+    },
+    {
+      title: 'Кейсы',
+      link: '/'
+    },
+    {
+      title: 'О нас',
+      link: '/'
+    },
+    {
+      title: 'Контакты',
+      link: '/'
+    },
+  ]
 
   return (
     <header className="header">
@@ -15,18 +37,37 @@ export default function Header() {
           </p>
         </div>
         <div className="header__marquee">
-          <ul className="header__marquee-list list">
-            {projectsArr.map((el) => (
-              <>
-                <li key={el} className="header__marquee-item">
-                  {el}
-                </li>
-                <span className="header__marquee-line">/</span>
-              </>
-            ))}
-          </ul>
+          <Marquee autoFill={true}>
+            <ul className="header__marquee-list list">
+              {projectsArr.map((el, i) => (
+                <React.Fragment key={i}>
+                  <li className="header__marquee-item" >
+                    {el}
+                  </li>
+                  <span className="header__marquee-line">/</span>
+                </React.Fragment>
+              ))}
+            </ul >
+          </Marquee>
         </div>
       </div>
-    </header>
+      <div className="header__bottom">
+        <Link className="header__link" to='/'>
+          <img src={logo} alt="CAPS logo" className="header__logo" />
+        </Link>
+        <ul className="header__menu-list list">
+          {
+            menuList.map((el) => (
+              <li key={el.title} className="header__menu-item">
+                <Link className="header__menu-link link" to={el.link}>
+                  {el.title}
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+        <button type="button" className="header__button button">заказать</button>
+      </div>
+    </header >
   )
 }
