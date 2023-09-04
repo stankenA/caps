161ccff72d17fb3131a3aa345e000d7aebf7b90b
stanken_a <artyemst3@mail.ru>
2023-09-04 14:25:18 +0300

    feat: add adaptive for services page

--- src/components/Services/CompleteCard.jsx
+++ src/components/Services/CompleteCard.jsx
@@ -8,7 +8,7 @@ export default function CompleteCard({ isCardComplete }) {
     <div className={`services__complete ${isCardComplete ? 'services__complete_active' : ''}`}>
       <div className="services__complete-left">
         <h2 className="services__title">Готово!</h2>
-        <p className="services__txt">
+        <p className="services__txt services__txt_complete">
           Спасибо за оставленную заявку! В скором времени мы с вами свяжемся, а пока предлагаем сыграть в игру: <br />
           <br />
           На нашем сайте спрятан промокод, который даст вам <span className="services__complete-discount">скидку 10%</span>  на ваш заказ. Удачи ;&#41;
