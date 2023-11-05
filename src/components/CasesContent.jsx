c005cd1bf0fd2ea429eee38896522385a83def42
stanken_a <artyemst3@mail.ru>
2023-11-05 16:07:17 +0300

    feat: add responsive for cases section

--- src/components/CasesContent.jsx
+++ src/components/CasesContent.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import casesMobImg from "../assets/images/cases-mob-img-1.png";
 
 export default function CasesContent({ caseObj, activeCard, index }) {
+  const [isContentOpened, setIsContentOpened] = useState(false);
+
   const isActive = activeCard === index;
   return (
     <div
@@ -9,9 +12,14 @@ export default function CasesContent({ caseObj, activeCard, index }) {
       }`}
     >
       <div className="cases__content-box">
-        <img src={caseObj.img} alt="Описание кейса" className="cases__img" />
+        <img src={caseObj.img} alt="Превью кейса" className="cases__img" />
         <div className="cases__container">
-          <div className="cases__txt-box">
+          <div
+            className={`cases__txt-box ${
+              isContentOpened ? "cases__txt-box_opened" : ""
+            }`}
+            onClick={() => setIsContentOpened(!isContentOpened)}
+          >
             <h3 className={`cases__subtitle cases__subtitle_${index + 1}`}>
               {caseObj.title}
             </h3>
@@ -28,7 +36,17 @@ export default function CasesContent({ caseObj, activeCard, index }) {
                 </li>
               ))}
             </ul>
+            <span
+              className={`cases__arrow ${
+                isContentOpened ? "cases__arrow_opened" : ""
+              }`}
+            ></span>
           </div>
+          <img
+            src={casesMobImg}
+            alt="Превью кейса"
+            className="cases__img-mobile"
+          />
           <a href={caseObj.link} className="cases__link">
             ознакомиться
           </a>
